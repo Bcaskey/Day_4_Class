0d839b61f1bb4444559b0aa1075ddda7d1d3d826
Bcaskey <bcaskeyu@charter.net>
2016-02-25 12:11:48 -0600

    Adding DOM js

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
+//below forces the function to run when content is loaded
+
+document.addEventListener('DOMContentLoaded', function () {
+    var element = document.getElementById('id-1');
+    element.style.color = 'red';
+});
+
 function checkAge() {
     var age = prompt('What is your age?');
     console.log('Hello, you are ' + age);
@@ -50,6 +57,10 @@ function subtract(op1, op2) {
     return op1 - op2;
 }
 
+function multiply(op1, op2) {
+    return op1 * op2;
+}
+
 function divide(op1, op2) {
     if (op2 != 0) {
         return op1 / op2;
@@ -58,6 +69,49 @@ function divide(op1, op2) {
     }
 }
 
-function multiiply(op1, op2) {
-    return op1 * op2;
+
+
+function calculate(operation) {
+    var op1 = document.getElementById('op1').value;
+    var op2 = document.getElementById('op2').value;
+    op1 = parseInt(op1, 10);
+    op2 = parseInt(op2, 10);
+    var result = 0;
+    switch (operation) {
+        case 'add':
+            result = add(op1, op2);
+            break;
+        case 'subtract':
+            result = subtract(op1, op2);
+            break;
+        case 'multiply':
+            result = multiply(op1, op2);
+            break;
+        case 'divide':
+            result = divide(op1, op2);
+            break;
+    }
+    console.log('The result is ' + result);
+}
+
+
+function addParagraph() {
+    // Setting up paragraph and text
+    var paragraph = document.createElement('p');
+    var text = document.createTextNode(new Date().toString());
+    paragraph.appendChild(text);
+    // paragraph.innerText = new Date().toString();   a duplicate way to do above
+    
+    // Modifying the paragraph
+    
+    //Two Ways --------
+    
+    //paragraph.style.fontFamily = 'sans-serif';
+    //paragraph.style.color = '#383838';
+    // below uses class rather than individual styles
+    paragraph.className = 'my-para';
+    
+    // Actually adding the paragraph to the DOM body
+    
+    document.body.appendChild(paragraph);
 }
